8933bcefcb2b79e4214dd3a9a25585824a86eeab
Allen Williams <allen.williams2@rockets.utoledo.edu>
2018-08-07 19:22:28 -0400

    Update nav.js

--- view/js/nav.js
+++ view/js/nav.js
@@ -1,6 +1,9 @@
 var nbar = document.createElement("nav");
+//navbar classes added here
 nbar.className="navbar navbar-dark bg-dark";
 nbar.innerHTML=(
+  //When changes to the navbar template are made, HTML should be put here
+  //as a long string with '\' at the end of each line.
   '<span class="navbar-brand mb-0 h1"><i class="fas fa-chess-knight"></i> \
   Home <span class="text-muted">Beta Version</span></span>');
 anchor = document.body.firstChild;
