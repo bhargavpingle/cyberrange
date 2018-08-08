35bdc7f32c68a1fb95c91e2053098d0590b94711
Allen Williams <allen.williams2@rockets.utoledo.edu>
2018-08-08 17:47:24 -0400

    Added risk dictionary
    Made dictionary to store chart data in preparation for database integration

--- view/js/risk.js
+++ view/js/risk.js
@@ -1,3 +1,16 @@
+var risk = {
+  //This dictionary should be populated with variables from the database
+  //changing these variables affects the charts
+  "Technology": 2,
+  "People": 10,
+  "Policies": 5,
+  "Process": 2,
+  "legal": 10,
+  "business model": 2,
+  "board expectations": 5,
+  "Political expectation": 2
+}
+
 window.onload =function(){
     var ctx = document.getElementById('currentRisk').getContext('2d');
     var ctx2 = document.getElementById('riskTolerance').getContext('2d');
@@ -18,7 +31,7 @@ window.onload =function(){
                 label: "My First dataset",
                 backgroundColor: ['#28A745','#17A2B8','#6C757D','#007BFF'],
                 // borderColor: 'rgb(255, 99, 132)',
-                data: [2, 10, 5, 2],
+                data: [risk["Technology"], risk["People"], risk["Policies"], risk["Process"]],
             }]
         },
 
@@ -37,12 +50,11 @@ window.onload =function(){
                 label: "My First dataset",
                 backgroundColor: ['#28A745','#6C757D','#17A2B8','#007BFF'],
                 // borderColor: 'rgb(255, 99, 132)',
-                data: [10, 2, 5, 2],
+                data: [risk["legal"], risk["business model"], risk["board expectations"], risk["Political expectation"]],
             }]
         },
 
         // Configuration options go here
         options: {}
-    });   
+    });
 }
-
